260f067ecb4a70e773a9694fb492c73e2d4040fd
Celal Karahan <celal51kara@gmail.com>
2024-02-22 00:45:02 +0300

    [Feat] add general settings  model

--- core/models.py
+++ core/models.py
@@ -1,9 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class GeneralSettings(models.Model):
-    name = models.CharField(default='',max_length=254,blank=True)
-    description = models.CharField(default='',max_length=254,blank=True)
-    parameter = models.CharField(default='',max_length=254,blank=True)
-    updated_date = models.DateTimeField(auto_now=True,blank=True)
-    created_date = models.DateTimeField(auto_now_add=True,blank=True)
+class GeneralSetting(models.Model):
+    name = models.CharField(default='',max_length=254,blank=True,verbose_name='Setting Name',help_text='Name of the setting')
+    description = models.CharField(default='',max_length=254,blank=True,verbose_name='Description',help_text='Description of the setting')
+    parameter = models.CharField(default='',max_length=254,blank=True,verbose_name='Parameter',help_text='Parameter name')
+    updated_date = models.DateTimeField(auto_now=True,blank=True,verbose_name='Updated Date',help_text='Date when the record was last updated')
+    created_date = models.DateTimeField(auto_now_add=True,blank=True,verbose_name='Created Date',help_text='Date when the record was created')
+    def __str__(self):
+        return f'General Setting: {self.name}'
+    class Meta:
+        verbose_name = "General Setting"
+        verbose_name_plural = "General Settings"
+        ordering=('name','parameter')
