005596ddd24a9d8d185d61b6d304d23c1674022a
Celal Karahan <celal51kara@gmail.com>
2024-04-07 02:35:28 +0300

    [feat] create message model

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,15 @@
 from django.db import models
-
+from core.models import AbstractBase
 # Create your models here.
+class Message(AbstractBase):
+    name = models.CharField(max_length=255, blank=True, verbose_name='Name')
+    email = models.EmailField(max_length=255, blank=True, verbose_name='Email')
+    subject = models.CharField(max_length=255, blank=True, verbose_name='Subject')
+    message = models.TextField(blank=True, verbose_name='Message')
+
+    def __str__(self):
+        return f'Message: {self.name}'
+    class Meta:
+        verbose_name = 'Message'
+        verbose_name_plural = 'Messages'
+        ordering = ['-created_date']
--- contact/views.py
+++ contact/views.py
@@ -1,11 +1,23 @@
 from django.http import JsonResponse
 from django.shortcuts import render
-
+from contact.models import Message
 # Create your views here.
 def contact_form(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        subject = request.POST.get('subject')
+        message = request.POST.get('message')
+        Message.objects.create(name=name, email=email, subject=subject, message=message)
+        success = True
+        message = 'Message sent successfully'
+    else:
+        success = False
+        message = 'Invalid request method'
+
     context = {
-        'success': True,
-        'message': 'Contact from sent succesfully.',
+        'success': success,
+        'message': message,
     }
     return JsonResponse(context)
 def contact(request):
